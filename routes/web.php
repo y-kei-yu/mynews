cda45d0dce0103363443f0d8d787a43462d14e63
”吉田侑平” <yyouhey617@gmail.com>
2020-06-29 14:53:02 +0000

    PHP/Laravel 09

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+//課題3　「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('XXX', 'AAAController@bbb');
+});
+//課題4　【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して
+//admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら 
+//ProfileController の edit Action に割り当てるように設定してください。
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('admin/profile/create', 'ProfileControler@add');
+    Route::get('admin/profile/edit', 'ProfileController@edit');
+});
+
