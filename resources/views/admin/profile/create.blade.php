6cd1e76197e6f10ea640d605b8ee91795109a648
”吉田侑平” <yyouhey617@gmail.com>
2020-07-02 05:18:50 +0000

    2020/07/02 PHP/Laravel 11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,6 +8,23 @@
         <title>My Profile</title>
     </head>
     <body>
-        <h1>add</h1>
+        <h1></h1>
     </body>
 </html>
+
+ {{-- layouts/profile.blade.phpを読み込む --}}
+    @extends('layouts.profile')
+    
+    {{-- profile.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
+    @section('title', 'Myプロフィール')
+    
+    {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+    @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+    @endsection
