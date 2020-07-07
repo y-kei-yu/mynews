b3a7b31d9148c7609c39a682863d0024ee66c242
”吉田侑平” <yyouhey617@gmail.com>
2020-07-07 08:03:11 +0000

    202/07/07 PHP Laravel 13

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -13,5 +13,11 @@ class NewsController extends Controller
      return view('admin.news.create');
  }
     
+    
+ public function create(Request $request)
+ {
+    //admin/news/createにリダイレクトする
+    return redirect('admin/news/create');
+ }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
 });
 
 //課題3　「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
@@ -27,13 +28,14 @@ Route::group(['prefix' => 'admin'], function() {
 //課題4　【応用】 前章でAdmin/ProfileControllerを作成し、add Action, edit Actionを追加しました。web.phpを編集して
 //admin/profile/create にアクセスしたら ProfileController の add Action に、admin/profile/edit にアクセスしたら 
 //ProfileController の edit Action に割り当てるように設定してください。
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-});
+
 
 Auth::routes();
 
